ebc08fe250e32973b77855be86813202f848e7d3
TLeborgne <leborgne@et.esiea.fr>
2017-03-03 17:29:36 +0100

    Fix tours joueurs et dictionnaire

--- src/main/java/fr/esiea/nourby_leborgne/gameDependencies/Dictionnary.java
+++ src/main/java/fr/esiea/nourby_leborgne/gameDependencies/Dictionnary.java
@@ -1,6 +1,7 @@
 package fr.esiea.nourby_leborgne.gameDependencies;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
@@ -10,35 +11,24 @@ import java.io.FileInputStream;
 public class Dictionnary {
 		private HashSet<String> dico;
 		
-		public Dictionnary(){
-			this.dico = new HashSet<String>();
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("nourby_leborgne/ressources/dico.txt"), "UTF-8")))
-		    {
-				String line;
-				while((line = br.readLine()) != null){
+	public Dictionnary() {
+		this.dico = new HashSet<String>();
+		String path = new File("").getAbsolutePath();
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(new FileInputStream(path + "\\src\\main\\ressources\\dico.txt"), "UTF-8"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				if (!notValid(line)) {
 					this.dico.add(this.simplify(line));
-				}//fin while
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e){
-				e.printStackTrace();
-			}
+				}
+			} // fin while
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-		public Dictionnary(String s){
-			this.dico = new HashSet<String>();
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(s), "UTF-8")))
-		    {
-				String line;
-				while((line = br.readLine()) != null){
-					this.dico.add(line);
-				}//fin while
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e){
-				e.printStackTrace();
-			}
-		}
+		displayDico();
+	}
 		
 		public String simplify(String input){//fonction qui sert à simplifier les lettres: é->e etc...
 			input = input.replace('é', 'e');
@@ -58,6 +48,11 @@ public class Dictionnary {
 
 	        return input;
 		}
+		
+		public final boolean notValid(String w)
+	    {
+			return w.contains(" ") || w.contains("'") || w.contains(".") || w.length()==0;
+	    }
 		
 		public boolean contains(String s){
 			return this.dico.contains(s);
--- src/main/java/fr/esiea/nourby_leborgne/gameEngine/Engine.java
+++ src/main/java/fr/esiea/nourby_leborgne/gameEngine/Engine.java
@@ -8,6 +8,7 @@ import fr.esiea.nourby_leborgne.gameDependencies.CommonJar;
 import fr.esiea.nourby_leborgne.gameDependencies.LetterBag;
 import fr.esiea.nourby_leborgne.gameDependencies.Player;
 import fr.esiea.nourby_leborgne.gameDependencies.WordsMade;
+import fr.esiea.nourby_leborgne.gameDependencies.Dictionnary;
 
 public class Engine {
 
@@ -15,161 +16,159 @@ public class Engine {
 	private LetterBag letterBag;
 	private CommonJar commonJar;
 	private WordsMade wordsMade;
+	private Dictionnary dico;
 	private int nbPlayer;
 	private boolean gameEnd;
-	
-	public Engine(int nP,int dif){//initialization du jeu
-		this.gameEnd=false;
-		this.nbPlayer=nP;
-		this.wordsMade=new WordsMade();
+
+	public Engine(int nP, int dif) {// initialization du jeu
+		this.gameEnd = false;
+		this.nbPlayer = nP;
+		this.wordsMade = new WordsMade();
 		this.letterBag = new LetterBag();
 		this.commonJar = new CommonJar();
-		this.playerList=new LinkedList<Player>();
-		for(int i = 1; i<=nP ; i++){
-			Player p = new Player("Player "+String.valueOf(i));
+		this.playerList = new LinkedList<Player>();
+		this.dico = new Dictionnary();
+		for (int i = 1; i <= nP; i++) {
+			Player p = new Player("Player " + String.valueOf(i));
 			playerList.add(p);
 		}
 	}
-	
-	public Player getPlayer(int i){
+
+	public Player getPlayer(int i) {
 		return this.playerList.get(i);
 	}
-	
-	public int getNbPlayer(){
+
+	public int getNbPlayer() {
 		return this.nbPlayer;
 	}
-	
-	public boolean isValid(String s){//vérifie si le mot est valide
-		for(int i=0; i<s.length() ; i++){
-			if(!this.commonJar.contains(String.valueOf(s.charAt(i)))){
+
+	public boolean isValid(String s) {// vérifie si le mot est valide
+		for (int i = 0; i < s.length(); i++) {
+			if (!this.commonJar.contains(String.valueOf(s.charAt(i)))) {
 				return false;
 			}
 		}
-		if(this.wordsMade.isInHistory(s)){
+		if (this.wordsMade.isInHistory(s)) {
 			return false;
 		}
 		return true;
 	}
-	
-	public void del(String s, Player p){//fonction qui supprime le mot d'un joueur
+
+	public void del(String s, Player p) {// fonction qui supprime le mot d'un
+											// joueur
 		p.delWord(s);
 	}
-	
-	public void add(String s, Player p){//fonction qui ajoute un mot à un joueur
+
+	public void add(String s, Player p) {// fonction qui ajoute un mot à un
+											// joueur
 		p.addWord(s);
 		this.commonJar.delLetter(s);
 		this.wordsMade.addWord(s);
 	}
-	
-	public String waitWord(){//attend qu'un mot soit écrit par un joueur
+
+	public String waitWord() {// attend qu'un mot soit écrit par un joueur
 		Scanner sc = new Scanner(System.in);
-		String str=" ";
+		String str = " ";
 		System.out.println("Veuillez saisir un mot :");
-		try{
-			//while (sc.hasNextLine()){
-			str = sc.nextLine();//}
-		}catch(InputMismatchException e){
-			
+		try {
+			// while (sc.hasNextLine()){
+			str = sc.nextLine();// }
+		} catch (InputMismatchException e) {
+
 		}
-		
-		System.out.println("Le mot choisit est: "+str);
+
+		System.out.println("Le mot choisit est: " + str);
 		return str;
 	}
 
-	public void checkEnd(){//vérifie si un joueur satisfait les conditions de fin
-		for(int i=0; i<this.nbPlayer ; i++){
-			System.out.println(this.playerList.get(i).getName()+" a "+this.playerList.get(i).getListSize()+" mots.");
-			if(this.playerList.get(i).getListSize()==10){
-				this.gameEnd=true;
+	public void checkEnd() {// vérifie si un joueur satisfait les conditions de
+							// fin
+		for (int i = 0; i < this.nbPlayer; i++) {
+			System.out.println(
+					this.playerList.get(i).getName() + " a " + this.playerList.get(i).getListSize() + " mots.");
+			if (this.playerList.get(i).getListSize() == 10) {
+				this.gameEnd = true;
 			}
 		}
 	}
-	
-	public void displayPlayers(){//affiche chaque joueur et ses mots
-		for(int i=0; i<this.nbPlayer; i++){
+
+	public void displayPlayers() {// affiche chaque joueur et ses mots
+		for (int i = 0; i < this.nbPlayer; i++) {
 			System.out.println(this.playerList.get(i).getName());
 			System.out.println("Words:");
 			this.playerList.get(i).showWords();
 			System.out.println("");
 		}
 	}
-	
-	public void displayLetters(){//affiches les lettres du pot commun
+
+	public void displayLetters() {// affiches les lettres du pot commun
 		System.out.print("Le pot de lettres contient: ");
 		this.commonJar.showLetters();
 		System.out.println("\n");
 	}
-	
-	public void displayEnd(){//écran de fin
+
+	public void displayEnd() {// écran de fin
 		System.out.println("Fin du jeu !");
 	}
-	
-	public String drawLetter(Player p){//fonction qui tire une lettre par joueur
-		System.out.println(p.getName()+" est en train de tirer une lettre...");
+
+	public String drawLetter(Player p) {// fonction qui tire une lettre par
+										// joueur
+		System.out.println(p.getName() + " est en train de tirer une lettre...");
 		this.commonJar.addLetter(this.letterBag.drawLetter());
-		System.out.println("Il a tiré "+this.commonJar.getLast());
+		System.out.println("Il a tiré " + this.commonJar.getLast());
 		return this.commonJar.getLast();
 	}
-	
-	
-	public int begin(){//choisit quel joueur commence
-		int indexP=0;
+
+	public int begin() {// choisit quel joueur commence
+		int indexP = 0;
 		int val;
-		val=this.letterBag.getLetterValue(this.drawLetter(this.playerList.get(indexP)));
-		for(int i=1; i<this.nbPlayer ;i++){
-			int tmp=this.letterBag.getLetterValue(this.drawLetter(this.playerList.get(i)));
-			if(this.letterBag.getLetterValue(this.commonJar.getLast())<val){
-				val=tmp;
-				indexP=i;
+		val = this.letterBag.getLetterValue(this.drawLetter(this.playerList.get(indexP)));
+		for (int i = 1; i < this.nbPlayer; i++) {
+			int tmp = this.letterBag.getLetterValue(this.drawLetter(this.playerList.get(i)));
+			if (this.letterBag.getLetterValue(this.commonJar.getLast()) < val) {
+				val = tmp;
+				indexP = i;
 			}
 		}
-		System.out.println("Le joueur qui commence est: "+this.playerList.get(indexP).getName());
+		System.out.println("Le joueur qui commence est: " + this.playerList.get(indexP).getName());
 		return indexP;
 	}
-	
-	public void tour(Player p){//fonction qui gère les tours
-		System.out.println("Tour de "+p.getName());
+
+	public void tour(Player p) {// fonction qui gère les tours
+		System.out.println("Tour de " + p.getName());
 		this.drawLetter(p);
 		this.drawLetter(p);
 		this.displayPlayers();
 		this.displayLetters();
 	}
-	
-	public int decideWord(String s, Player p, int indexP){//fonction qui décide quoi faire avec le mot
-		if(s.equals(null)){
-			if(indexP==0){
-				indexP=1;
-			}else{
-				indexP=0;
-			}
-			System.out.println("Le joueur a passé son tour !");
+
+	public int decideWord(String s, Player p, int indexP) {// fonction qui
+															// décide quoi faire
+		if (!dico.notValid(s) && this.isValid(s) && dico.contains(s)) {
+			this.add(s, p);
+			return indexP;
 		}
-		else{
-			if(this.isValid(s)){
-				this.add(s, p);
-			}else{
-				if(indexP==0){
-					indexP=1;
-				}else{
-					indexP=0;
-				}
-				System.out.println("Le mot choisit n'est pas valide ! Au tour de l'autre joueur!");
-			}
+		if (indexP == 0) {
+			indexP = 1;
+		} else {
+			indexP = 0;
 		}
+		System.out.println("Le mot choisit n'est pas valide ! Au tour de l'autre joueur!");
 		return indexP;
 	}
-	
-	public void gameStart(){//fonction qui gère les tours de jeu
-		this.gameEnd=false;
-		int indexCurrentPlayer=this.begin();
-		Player currentPlayer=this.playerList.get(indexCurrentPlayer);
-		String word="";
-		do{
+
+	public void gameStart() {// fonction qui gère les tours de jeu
+		this.gameEnd = false;
+		int indexCurrentPlayer = this.begin();
+		Player currentPlayer = this.playerList.get(indexCurrentPlayer);
+		String word = "";
+		do {
 			this.tour(currentPlayer);
-			word=this.waitWord();
-			currentPlayer = this.playerList.get(this.decideWord(word, currentPlayer, indexCurrentPlayer));
+			word = this.waitWord();
+			indexCurrentPlayer = decideWord(word, currentPlayer, indexCurrentPlayer);
+			currentPlayer = this.playerList.get(indexCurrentPlayer);
 			this.checkEnd();
-		}while(this.gameEnd!=true);
+		} while (this.gameEnd != true);
 		this.displayEnd();
 	}
 }
